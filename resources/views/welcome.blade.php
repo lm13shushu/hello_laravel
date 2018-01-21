2bf4804f5969337d5c6a0967b5362ffe4e486bec
Your Name <742862703@qq.com>
2018-01-21 14:38:59 +0000

    Say something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    HELLO WORLD
+                    HELLO WORLD~
                 </div>
             </div>
         </div>
